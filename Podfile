429ad20895f0dd38290527d50940d1be1b2d4ebd
Ji Oh Yoo <jioh.yoo@gmail.com>
2016-02-29 19:43:02 -0800

    initial project setup

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'never-go-bad' do
+	pod "AFNetworking"
+	pod "BDBOAuth1Manager"
+	pod "MBProgressHUD"
+end
+
+target 'never-go-badTests' do
+
+end
+
+target 'never-go-badUITests' do
+
+end
+
---
+++ never-go-bad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  never-go-bad
+//
+//  Created by Ji Oh Yoo on 2/29/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
