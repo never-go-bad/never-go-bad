5a2ec23304f90e640d477955a407209d6c585be7
Ji Oh Yoo <jioh.yoo@gmail.com>
2016-03-09 00:46:25 -0800

    committing food list for history before deleting, come back here to retreive the original list

---
+++ never-go-bad/FoodDictionaryService.swift
@@ -0,0 +1,13 @@
+//
+//  FoodDictionaryService.swift
+//  never-go-bad
+//
+//  Created by Ji Oh Yoo on 3/9/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class FoodDictionaryService: NSObject {
+
+}
