439b104d7ad722ddeea0b5117d644c7be09cc74e
Ji Oh Yoo <jioh.yoo@gmail.com>
2016-03-20 21:50:55 -0700

    custom tab bar works

---
+++ never-go-bad/MainTabBarButton.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabBarButton.swift
+//  never-go-bad
+//
+//  Created by Ji Oh Yoo on 3/20/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarButton: UIView {
+    
+    var buttonImage: UIImage?
+    var title: String?
+    var state: Bool?
+        
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
